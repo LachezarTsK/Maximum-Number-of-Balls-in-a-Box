33ad9b9f96750cd84f8d65b32672b4be2ea068e3
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-06 21:27:19 +0200

    Create Solution.kt

---
+++ Solution.kt
@@ -0,0 +1,32 @@
+
+using System;
+
+public class Solution
+{
+    private static readonly int[] RANGE_OF_LIMITS = { 1, (int)Math.Pow(10, 5) };
+
+    public int CountBalls(int lowLimit, int highLimit)
+    {
+        int[] frequency = new int[calculateSumOfDigits(RANGE_OF_LIMITS[1] - 1) + 1];
+        int maxFrequency = 0;
+
+        for (int value = lowLimit; value <= highLimit; ++value)
+        {
+            int sumOfDigits = CalculateSumOfDigits(value);
+            maxFrequency = Math.Max(maxFrequency, ++frequency[sumOfDigits]);
+        }
+
+        return maxFrequency;
+    }
+
+    private int CalculateSumOfDigits(int value)
+    {
+        int sumOfDigits = 0;
+        while (value > 0)
+        {
+            sumOfDigits += value % 10;
+            value /= 10;
+        }
+        return sumOfDigits;
+    }
+}
